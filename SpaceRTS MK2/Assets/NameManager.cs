7fd6df61a6a1f447bab4a9cbea46071395977084
xnenaixyinaa <rxjohnston@gmail.com>
2017-08-19 22:39:38 -0800

    added numerali di romani del nome degli shippi

--- SpaceRTS MK2/Assets/NameManager.cs
+++ SpaceRTS MK2/Assets/NameManager.cs
@@ -1,9 +1,23 @@
 ﻿using System.Collections.Generic;
 //using UnityEngine;
 using System.IO;
-
+using System.Text;
+//public enum RomanNumeral
+//{
+//    I = 1,
+//    II,
+//    III,
+//    IV,
+//    V,
+//    VI,
+//    VII,
+//    VIII,
+//    IX,
+//    X
+//}
 public class NameManager// : MonoBehaviour
 {
+    static readonly string[] RomanNumerals = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X" };
     static string dir = Directory.GetCurrentDirectory() + "\\Text Files";
     static string file = dir + "\\Ship Names.txt";
     static List<string> names;
@@ -25,8 +39,9 @@ public class NameManager// : MonoBehaviour
                 names.Add(sr.ReadLine());
             while (!sr.EndOfStream);
         }
+
 		foreach (string n in names) {
-			Frequency.Add (names, 0);
+			Frequency.Add (n, 0);
 		}
     }
 
@@ -34,7 +49,8 @@ public class NameManager// : MonoBehaviour
     {
 		
         System.Random rand = new System.Random();
-		string name = names[rand.Next(0, names.Count)];
+        //string name = names[rand.Next(0, names.Count)];
+        string name = AssignNumeral(names[rand.Next(0, names.Count)]) ?? names[rand.Next(0, names.Count)];
         names.Remove(name);
         usedNames.Add(name);
 		CheckDic (name);
@@ -45,8 +61,14 @@ public class NameManager// : MonoBehaviour
 		Frequency [name]++;
 	}
 
-	static public void RecycleName(string name){
+	static public void RecycleName(string name)
+    {
 		usedNames.Remove (name);
 		names.Add (name);
 	}
+
+    static public string AssignNumeral(string name)
+    {
+        return Frequency[name] == 0 ? null : name += " " + RomanNumerals[Frequency[name]];
+    }
 }
