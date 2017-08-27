55bbd754e59e0ce19f5830f46e5750a10a8136c8
xnenaixyinaa <rxjohnston@gmail.com>
2017-08-27 01:36:25 -0800

    added mines to asteroides del tacos

--- SpaceRTS MK2/Assets/Asteroid.cs
+++ SpaceRTS MK2/Assets/Asteroid.cs
@@ -5,59 +5,62 @@ using UnityEngine.UI;
 using System.Text;
 using UnityEngine.Events;
 
-public class Asteroid : MonoBehaviour, IMineable {
+public class Asteroid : MonoBehaviour, IMineable
+{
 
-	public ResourceDeposit deposit;
+    public ResourceDeposit deposit;
 
-	public Text stockpileText;
+    public Text stockpileText;
+    public List<Mine> mines;
+
+    // Use this for initialization
+    void Awake()
+    {
+
+        deposit = new ResourceDeposit((RawResources)Random.Range(1, 6));
+        gameObject.name = deposit.resource.ToString() + " Asteroid";
+        Collections.Mineable.Add(this);
+        mines = new List<Mine>();
+    }
 
 
-	// Use this for initialization
-	void Awake () {
 
-		deposit = new ResourceDeposit ((RawResources)Random.Range (1, 6));
-		gameObject.name = deposit.resource.ToString () + " Asteroid";
-		Collections.Mineable.Add (this);
-	}
+    public float Mine(RawResources r, float amount)
+    {
+        if (deposit.resource != r)
+            return 0f;
+        amount = amount * deposit.access;
+        if (amount <= deposit.amount)
+        {
+            deposit.amount -= amount;
+            UpdateText();
+            return amount;
+        }
+        return 0f;
+    }
 
-
-
-	public float Mine(RawResources r,float amount){
-		if (deposit.resource != r)
-			return 0f;
-		amount = amount * deposit.access;
-		if (amount <= deposit.amount) {
-			deposit.amount -= amount;
-			UpdateText ();
-			return amount;
-		}
-		return  0f;
-	}
-
-	void UpdateText(){
+    void UpdateText()
+    {
         StringBuilder sb = new StringBuilder();
         //stockpileText.text = "Resources||\n";
         sb.AppendLine("Resources||");
-		foreach (Mine m in GetComponents<Mine>()) {
+        foreach (Mine m in mines)
+        {
             //stockpileText.text += string.Format ("{0}: {1} \n", m.resource.ToString (), m.Stockpile );
             sb.AppendFormat("{0}: {1}\n", m.resource.ToString(), m.Stockpile);
-			}
+        }
         stockpileText.text = sb.ToString();
-		}
+    }
 
 
-	void Update(){
-		}
+    void Update()
+    {
+    }
 
     [SerializeField]
     public void ConstructMiningFacility()
     {
         Debug.Log("Constructed a mining Facility");
-		ConstructionRequests r = new ConstructionRequests (new Vector3(transform.position.x +5f,0f,transform.position.y));
+        ConstructionRequests r = new ConstructionRequests(new Vector3(transform.position.x + 5f, 0f, transform.position.y));
     }
 }
-
-
-
-
-
